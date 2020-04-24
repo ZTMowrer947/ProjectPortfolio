b35223f79fa28655aec09111137d32a39e6b248f
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-24 14:26:04 -0400

    Personalize gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Zack Mowrer`,
+        description: `Witness the coding prowess manifested within the projects from the child prodigy of the Mountain State.`,
+        author: `@zackmowrer`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `zackm-portfolio`,
+                short_name: `zackm`,
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
