429ed8b60cd0aa637ab79b3c4f965932d2bf0393
Zackery Mowrer <Zackmowrer@gmail.com>
2020-05-07 20:28:24 -0400

    Remove placeholder markup in app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app.title).toEqual('project-portfolio');
     });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain(
-            'project-portfolio app is running!'
-        );
-    });
 });
