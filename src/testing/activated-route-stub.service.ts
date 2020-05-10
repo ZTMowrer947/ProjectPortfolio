ad547bcba866673c995a5b7d90d6eaeed3139d59
Zackery Mowrer <Zackmowrer@gmail.com>
2020-05-10 16:45:44 -0400

    Update ActivatedRouteStub to allow data mocking

--- src/testing/activated-route-stub.service.ts
+++ src/testing/activated-route-stub.service.ts
@@ -5,20 +5,27 @@ import { ReplaySubject } from 'rxjs';
  * An ActivateRoute test double with a `paramMap` observable.
  * Use the `setParamMap()` method to add the next `paramMap` value.
  */
-export class ActivatedRouteStub {
+export class ActivatedRouteStub<Data = unknown> {
     // Use a ReplaySubject to share previous values with subscribers
     // and pump new values into the `paramMap` observable
-    private subject = new ReplaySubject<ParamMap>();
+    private paramSubject = new ReplaySubject<ParamMap>();
+    private dataSubject = new ReplaySubject<Data>();
 
-    constructor(initialParams?: Params) {
+    constructor(initialParams?: Params, initialData?: Data) {
         this.setParamMap(initialParams);
+        this.setData(initialData);
     }
 
     /** The mock paramMap observable */
-    readonly paramMap = this.subject.asObservable();
+    readonly paramMap = this.paramSubject.asObservable();
+    readonly data = this.dataSubject.asObservable();
 
     /** Set the paramMap observables's next value */
     setParamMap(params?: Params) {
-        this.subject.next(convertToParamMap(params));
+        this.paramSubject.next(convertToParamMap(params));
+    }
+
+    setData(data?: Data) {
+        this.dataSubject.next(data);
     }
 }
