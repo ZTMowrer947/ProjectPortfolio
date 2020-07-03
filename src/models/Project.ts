895b07ae4be6abf586530fc1420b211c2dc71917
Zackery Mowrer <Zackmowrer@gmail.com>
2020-07-03 08:57:55 -0400

    Add Project model and service

---
+++ src/models/Project.ts
@@ -0,0 +1,14 @@
+// Interface
+interface Project {
+    id: number;
+    name: string;
+    description: string;
+    technologies: string[];
+    liveLink: string | null;
+    githubLink: string;
+    landingImageUrl: string;
+    galleryImageUrls: string[];
+}
+
+// Exports
+export default Project;
---
+++ src/services/ProjectService.ts
@@ -0,0 +1,29 @@
+// Imports
+import Project from '../models/Project';
+import { projects as projectData } from '../../data.json';
+
+// Project data
+const projects: Project[] = projectData.map((project) => ({
+    id: project.id,
+    name: project.project_name,
+    description: project.description,
+    technologies: project.technologies,
+    liveLink: project.live_link ?? null,
+    githubLink: project.github_link,
+    landingImageUrl: project.image_urls.landing,
+    galleryImageUrls: project.image_urls.gallery,
+}));
+
+// Service
+class ProjectService {
+    public static async getList(): Promise<Project[]> {
+        return Promise.resolve(projects);
+    }
+
+    public static async get(id: number): Promise<Project | undefined> {
+        return Promise.resolve(projects.find((project) => project.id === id));
+    }
+}
+
+// Exports
+export default ProjectService;
