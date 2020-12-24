af6b33c0f5ae9254f8d5a4890a22ec0417309da6
Zackery Mowrer <Zackmowrer@gmail.com>
2020-12-24 11:42:20 -0500

    Modify App component to use @ alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 // Imports
-import '../app.scss';
+import '@/app.scss';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { FC } from 'react';
 import Container from 'react-bootstrap/Container';
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 // App component
 const CustomApp: FC<AppProps> = ({ Component, pageProps }) => {
